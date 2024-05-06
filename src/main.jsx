7fec2f327fd24faa3b376f32b2ceaeeadd068935
Carlos Eduardo <138811560+Carlosalves18@users.noreply.github.com>
2024-05-06 18:27:58 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './ATT/App'
+import Form from './ATT/Form/Form'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Form/>
+   <App/>
+  </React.StrictMode>,
+)
